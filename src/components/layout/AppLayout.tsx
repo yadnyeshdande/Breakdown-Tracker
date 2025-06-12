d335d525523edc03f8b2bc11ee42410d9ea6b572
Pseudo Fury <endofthegame1111@gmail.com>
2025-06-12 17:53:12 +0000

    the side menu drawer and ui is overlapping. Also you have to scroll down

--- src/components/layout/AppLayout.tsx
+++ src/components/layout/AppLayout.tsx
@@ -1,3 +1,4 @@
+
 import React from "react";
 import {
   Sidebar,
@@ -27,12 +28,10 @@ export function AppLayout({ children }: { children: React.ReactNode }) {
           <SidebarNav />
         </SidebarContent>
       </Sidebar>
-      <div className="flex flex-col sm:gap-4 sm:py-4 sm:pl-14 peer-data-[collapsible=icon]:sm:pl-14 peer-data-[collapsible=offcanvas]:sm:pl-0 transition-[padding] duration-200 ease-linear">
+      <div className="flex flex-col flex-1 transition-[padding] duration-200 ease-linear peer-data-[state=expanded]:sm:pl-[var(--sidebar-width)] peer-data-[state=collapsed]:peer-data-[collapsible=icon]:sm:pl-[var(--sidebar-width-icon)] peer-data-[collapsible=offcanvas]:sm:pl-0">
          <Header />
-        <SidebarInset>
-          <main className="flex-1 p-4 sm:px-6 sm:py-0 md:gap-8 bg-background">
+        <SidebarInset className="flex-1 bg-background p-4 sm:px-6 sm:pb-6 sm:pt-[calc(theme(spacing.16)_+_theme(spacing.4))]">
             {children}
-          </main>
         </SidebarInset>
       </div>
     </div>
--- src/components/ui/sidebar.tsx
+++ src/components/ui/sidebar.tsx
@@ -1,3 +1,4 @@
+
 "use client"
 
 import * as React from "react"
@@ -322,7 +323,7 @@ const SidebarInset = React.forwardRef<
     <main
       ref={ref}
       className={cn(
-        "relative flex min-h-svh flex-1 flex-col bg-background",
+        "relative flex min-h-0 flex-1 flex-col bg-background", // Changed min-h-svh to min-h-0
         "peer-data-[variant=inset]:min-h-[calc(100svh-theme(spacing.4))] md:peer-data-[variant=inset]:m-2 md:peer-data-[state=collapsed]:peer-data-[variant=inset]:ml-2 md:peer-data-[variant=inset]:ml-0 md:peer-data-[variant=inset]:rounded-xl md:peer-data-[variant=inset]:shadow",
         className
       )}
