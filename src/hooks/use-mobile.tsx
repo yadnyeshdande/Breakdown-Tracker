e602ca64f676c404f7a61a3652addc4270ec96c0
Pseudo Fury <endofthegame1111@gmail.com>
2025-06-12 17:41:08 +0000

    abc

--- src/hooks/use-mobile.tsx
+++ src/hooks/use-mobile.tsx
@@ -2,6 +2,8 @@ import * as React from "react"
 
 const MOBILE_BREAKPOINT = 768
 
+let a = 5
+
 export function useIsMobile() {
   const [isMobile, setIsMobile] = React.useState<boolean | undefined>(undefined)
 
@@ -17,3 +19,5 @@ export function useIsMobile() {
 
   return !!isMobile
 }
+
+
